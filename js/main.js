23347a21578b0bd162540c0b8a789d844e8bc3c0
eralpkor <eralp.ny@gmail.com>
2019-07-31 13:37:37 -0400

    media query for about page

--- js/main.js
+++ js/main.js
@@ -8,3 +8,31 @@ function expandNav() {
     nav.className = 'navigation';
   }
 }
+
+
+// window.onscroll = () => {
+//   const nav = document.querySelector('#nav');
+//   if (this.scrollY <= 10) nav.className = 'navigation';
+//   else nav.className = 'changeColor';
+// };
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var test = document.querySelector('.test');
+  var copy = document.querySelector('.copy-right');
+
+
+  function resize() {
+    if (window.innerWidth <= 500) {
+      test.classList.add('mobile');
+      // test.classList.toggle('remove');
+      copy.classList.add('remove');
+    } else {
+      test.classList.remove('mobile');
+      copy.classList.remove('remove');
+    }
+  }
+  // For performance reasons, this method should be 
+  // "debounced" so that it doesn't have to execute 
+  // on every resize event, only when you're done resizing.
+  window.onresize = resize;
+});
